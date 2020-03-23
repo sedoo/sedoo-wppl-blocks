2b51c3f3e3561fd30543b7ad0e0f9c24ff6b1d5f
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-03-23 10:03:19 +0100

    tag 0.2.1

--- sedoo-wppl-blocks.php
+++ sedoo-wppl-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sedoo - Blocks
  * Description: Permet l'ajout de différents blocks
- * Version: 0.2.0
+ * Version: 0.2.1
  * Author: Nicolas Gruwe 
  * GitHub Plugin URI: sedoo/sedoo-wppl-blocks
  * GitHub Branch:     master
