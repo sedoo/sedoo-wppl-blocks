b50fd85a8a40d12d21d5221dd5baa00688464edc
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-04-22 17:49:01 +0200

    advanced mode beta apirest block

---
+++ blocks/sedoo-apirest/js/populatecpt.js
@@ -0,0 +1,95 @@
+    //////
+    // REMPLIR LE CHAMPS CPT EN FONCTION DU SITE
+    //////
+    jQuery(document).ready(function(){
+		jQuery(document).on('change', '[data-key="field_5e9eb8bab097a"] .acf-input select', function(e) {
+            idwebsite = jQuery(this).val();
+            jQuery.ajax({
+                url: ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
+                dataType:'text', //or HTML, JSON, etc.
+                data: {
+                    'action': 'sedoo_labtools_acf_populate_cptlist',
+                    'website' : idwebsite
+                },
+                success:function(results) {
+                    // le results me renvoie deux tableaux que je veux traiter differement, le resultat et le $tableau_ctx_par_cpt
+                    results = JSON.parse(results);
+                    resultats = results[0];
+                    tableau_taxonomies = results[1];
+                    jQuery('.acf-field-5e9ec0daae508 select').empty();
+                    jQuery('.acf-field-5e9ef4bfb9e24 select').empty();
+                    jQuery('.acf-field-5e9efed45552f select').empty();
+                    jQuery('.acf-field-5e9ec0daae508 select').append('<option value=""> Selectionner un type de contenu </option>');
+                    for (const property in resultats) {
+                        jQuery('.acf-field-5e9ec0daae508 select').append('<option value="'+property+'">'+resultats[property]+'</option>');
+                    }
+
+                    //////
+                    // REMPLIR LE CHAMPS CTX EN FONCTION DU CPT
+                    //////
+                    jQuery(document).on('change', '[data-key="field_5e9ec0daae508"] .acf-input select', function(e) {
+                        cptchoix = jQuery(this).val();
+                        jQuery.ajax({
+                            url: ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
+                            dataType:'text', //or HTML, JSON, etc.
+                            data: {
+                                'action': 'sedoo_labtools_acf_populate_ctxlist',
+                                'cpt' : cptchoix,
+                                'taxo_tableau' : tableau_taxonomies
+                            },
+                            success:function(taxoducpt) {
+                                taxoducpt = JSON.parse(taxoducpt);
+                                jQuery('.acf-field-5e9ef4bfb9e24 select').empty();
+                                jQuery('.acf-field-5e9efed45552f select').empty();
+                                jQuery('.acf-field-5e9ef4bfb9e24 select').append('<option value=""> Selectionner une taxonomie </option>');
+                                function addCtxToSelect(ctx) {
+                                    jQuery('.acf-field-5e9ef4bfb9e24 select').append('<option value="'+ctx+'">'+ctx+'</option>');
+                                }
+                                taxoducpt.forEach(addCtxToSelect);
+                                console.log(taxoducpt);
+
+                                //////
+                                // REMPLIR LE CHAMPS TERM EN FONCTION DU CTX
+                                //////
+                                jQuery(document).on('change', '[data-key="field_5e9ef4bfb9e24"] .acf-input select', function(e) {
+                                    ctxchoix = jQuery(this).val();
+                                    jQuery.ajax({
+                                        url: ajaxurl, // or example_ajax_obj.ajaxurl if using on frontend
+                                        dataType:'text', //or HTML, JSON, etc.
+                                        data: {
+                                            'action': 'sedoo_labtools_acf_populate_termlist',
+                                            'ctx' : ctxchoix
+                                        },
+                                        success:function(terms_list) {
+                                            terms = JSON.parse(terms_list);
+                                            jQuery('.acf-field-5e9efed45552f select').empty();
+                                            jQuery('.acf-field-5e9efed45552f select').append('<option value=""> Selectionner un terme </option>');
+                                            for (const property_term in terms) {
+                                                jQuery('.acf-field-5e9efed45552f select').append('<option value="'+property_term+'">'+terms[property_term]+'</option>');
+                                            }
+                                        },
+                                        error: function(errorThrown){
+                                            console.log(errorThrown);
+                                        }
+                                    });  
+                        
+                                });
+
+
+                            },
+                            error: function(errorThrown){
+                                console.log(errorThrown);
+                            }
+                        });  
+            
+                    });
+
+                },
+                error: function(errorThrown){
+                    console.log(errorThrown);
+                }
+            });  
+
+		});
+	});
+	
--- blocks/sedoo-apirest/template-parts/blocks/apirest/apirest.php
+++ blocks/sedoo-apirest/template-parts/blocks/apirest/apirest.php
@@ -1,12 +1,30 @@
 <?php
 
-$url = get_field('url_de_recuperation');
-$json = file_get_contents($url);
-$donnees = json_decode($json,true);
-$zonetitle = get_field('apiresttitle');
+$mode_edition = get_field('mode_dedition');
 
-echo '<h2>'.$zonetitle.'</h2>';
-echo '<section role="listNews" class="post-wrapper sedoo-labtools-listCPT">';
+
+// si le mode d'édition est  en mode avancé
+if($mode_edition == true) {
+    $url = get_field('url_de_recuperation');
+} else {
+    $site = get_field('site_a_recuperer');
+    $site_url = get_blog_details($site);
+    $cpt = get_field('contenus_a_recuperer');
+    $ctx = get_field('taxonomie_a_recuperer');
+    $term = get_field('term_a_recuperer');
+    $url = $site_url->siteurl.'/wp-json/wp/v2/'.$cpt.'?'.$ctx.'='.$term;
+
+        echo $url;
+}
+
+$json = file_get_contents($url);
+$donnees = json_decode($json,true); 
+
+$zonetitle = get_field('sedoo-apirest-block-list-title');
+    
+echo '<h2>'.__($zonetitle, 'sedoo-wpth-labs').'</h2>';
+echo '<section role="listNews" class="post-wrapper sedoo-labtools-listCPT">'; 
+
 foreach($donnees as $donnee) {
     $url = $donnee['link'];
     $title = $donnee['title']['rendered'];
