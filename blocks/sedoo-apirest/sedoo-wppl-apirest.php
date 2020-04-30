d709f801caf25c852fcecb95145c19f274ee4292
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-04-30 18:06:51 +0200

    ajout de commentaires utiles pour la compréhensiion après ce long week end

--- blocks/sedoo-apirest/sedoo-wppl-apirest.php
+++ blocks/sedoo-apirest/sedoo-wppl-apirest.php
@@ -78,18 +78,18 @@ add_action( 'wp_ajax_sedoo_labtools_acf_populate_cptlist', 'sedoo_labtools_acf_p
 //
 function sedoo_labtools_acf_populate_ctxlist() {
     if ( isset($_REQUEST) ) {
-        $cpt = $_REQUEST['cpt'];
-        $taxo_tableau = $_REQUEST['taxo_tableau'];
-        $tableau_xtx_name_restbase;
+        $cpt = $_REQUEST['cpt']; // le cpt envoyée par le js (choisie par l'user)
+        $taxo_tableau = $_REQUEST['taxo_tableau']; // le tableau des taxonomies par cpt envoyé par le js
+        $tableau_ctx_name_restbase; // la tableau que j'enverrais dans le select ctx avec comme champs : name => rest_base
         // je vais recuperer le name de chaque taxo plutot que le restbase
         foreach($taxo_tableau[$cpt] as $cpt_rest_base) {
-                $taxo_url_for_name = $_SESSION['siteurl'].'/wp-json/wp/v2/taxonomies/'.$cpt_rest_base;
+                $taxo_url_for_name = $_SESSION['siteurl'].'/wp-json/wp/v2/taxonomies/'.$cpt_rest_base; // je butine sur l'url du cpt 
                 $json_for_taxo_name = file_get_contents($taxo_url_for_name);
                 $json_for_taxo_name = json_decode($json_for_taxo_name,true);  
-                $tableau_xtx_name_restbase[$json_for_taxo_name['name']] = $cpt_rest_base;
+                $tableau_ctx_name_restbase[$json_for_taxo_name['name']] = $cpt_rest_base; // je peuple le tableau que j'enverrai au js
         }
 
-        echo json_encode($tableau_xtx_name_restbase);
+        echo json_encode($tableau_ctx_name_restbase);
     }
     die();
 }
@@ -99,34 +99,28 @@ add_action( 'wp_ajax_sedoo_labtools_acf_populate_ctxlist', 'sedoo_labtools_acf_p
 //
 // la fonction qui remplis le select term en fonction du ctx. 
 // - je recupere le slug de la ctx envoyé depuis js
-// - je butine dans toutes les taxo afin d'avoir une liste de toutes les ctx (a optimiser) et les met dans un tableau
-// - je recupere dans ce tableau le rest_base (car il était différent du slug dans 0.8.2 de labtools, normalement corrigé en 0.8.3) de la ctx envoyé depuis le js 
-// - je butine sur cette taxo et en recupere la liste des termes
 // - je fais mon tableau de terme
 // j'encode et j'envoie mon tableau
 //
 function sedoo_labtools_acf_populate_termlist() {
     if ( isset($_REQUEST) ) {
-        $ctx = $_REQUEST['ctx'];
-        $cpt = $_REQUEST['cpt'];
-        $taxo_url = $_SESSION['siteurl'].'/wp-json/wp/v2/taxonomies';
+        $ctx = $_REQUEST['ctx']; // le ctx envoyé par js
+        $cpt = $_REQUEST['cpt']; // le cpt envoyé par js
+        $taxo_url = $_SESSION['siteurl'].'/wp-json/wp/v2/taxonomies'; // - je butine dans toutes les taxo afin d'avoir une liste de toutes les ctx (a optimiser) et les met dans un tableau
         $termlistjson = file_get_contents($taxo_url);
         $terms = json_decode($termlistjson,true);  
-        $cpt_rest_base = $_SESSION['link_slug_rest_base_sedoo_cpt_name'];
-        $taxo_api_name = $terms[$ctx]['rest_base'];
-        $taxo_selectionnee_url = $taxo_url = $_SESSION['siteurl'].'/wp-json/wp/v2/'.$taxo_api_name;
-
+        $taxo_api_name = $terms[$ctx]['rest_base']; // je chope le rest_base de la taxo envoyée par le js (choisie par l'user)
+        $taxo_selectionnee_url = $taxo_url = $_SESSION['siteurl'].'/wp-json/wp/v2/'.$taxo_api_name; // - je butine sur l'url de la taxo avec le rest_base et en recupere la liste des termes
         $term_list = file_get_contents($taxo_selectionnee_url);
         $term_list = json_decode($term_list,true); 
         foreach($term_list as $term_ctx) {
-
             // on fait un truc propre : afficher le nombre de contenu correspondant à chaque terme (c'est possible donc on le fait)
-            $url_decompte = $_SESSION['siteurl'].'/wp-json/wp/v2/'.$cpt.'?'.$taxo_api_name.'='.$term_ctx['id'];
-            $decomptejson = file_get_contents($url_decompte);
-            $tab_decompte = json_decode($decomptejson, true);
+            $url_decompte = $_SESSION['siteurl'].'/wp-json/wp/v2/'.$cpt.'?'.$taxo_api_name.'='.$term_ctx['id']; // je fouille sur l'url du terme en fonction de taxo  + cpt
+            $decomptejson = file_get_contents($url_decompte); // je sors le contenu
+            $tab_decompte = json_decode($decomptejson, true); // je commence a parcourir pour compter
             $i = 0;
-            foreach($tab_decompte as $t) { $i++; }            
-            $tableau_term[$term_ctx['id']] = $term_ctx['name'].' ('.$i.')';
+            foreach($tab_decompte as $t) { $i++; }            // je finis de compter 
+            $tableau_term[$term_ctx['id']] = $term_ctx['name'].' ('.$i.')';  // j'affiche le name du term avec a coté le nombre de contenu entre parenthèses
         }
         echo json_encode($tableau_term);
     }
@@ -151,3 +145,7 @@ add_action( 'wp_ajax_sedoo_labtools_acf_populate_termlist', 'sedoo_labtools_acf_
 //  A l'édition les données ne sont pas chargée en mode pas à pas
 //      - en temps réel après l'avoir posé ok
 //      - a l'édition post création aussi mais on doit refaire tous les champs
+//
+// édition normale doit remplir le champ url pour simplifier l'affichage
+// l 32 populatecpt.js (choisir dans le tableau de ctx par cpt en fonction du cpt qui est choisi, et n'envoyer que ca au php pour moins faire travailler le php)
+// ll 125 à 129 sur sedoo-wppl-apirest.php - le décompte peut il se faire sans file get content, json decode et foreach ?
