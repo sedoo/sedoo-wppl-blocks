eb85ca4294f41fef78052a6fe7b285a97987ef8a
Pierre Vert <pierr65@gmail.com>
2021-11-17 16:13:24 +0100

    add date on grid-noimage for event CPT list

--- template-parts/content-grid-noimage.php
+++ template-parts/content-grid-noimage.php
@@ -21,6 +21,11 @@
                 ?>
                 <p><?php the_date('M / d / Y') ?></p>
             <?php endif; ?>
+            <?php
+            if ( 'tribe_events' === get_post_type() ) :
+                ?>
+                <p><?php echo tribe_get_start_date(get_the_ID(), false, 'd M Y - g:i'); ?></p>
+            <?php endif; ?>
             <a href="<?php the_permalink(); ?>"><?php echo __('Read more', 'sedoo-wpth-labs'); ?> →</a>
         </footer><!-- .entry-footer -->
     </div>
